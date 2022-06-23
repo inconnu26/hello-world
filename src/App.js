c695eb2e1348dbf386175131a4462d4653992f38
inconnu26 <bibas.jonathan@gmail.com>
2022-06-23 15:39:17 +0200

    more tests

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
-import Hello from "./components/Hello";
-import Counter from "./components/Counter";
-import Destructor from "./components/Destructor";
-import FunctionClick from "./components/FunctionClick";
-import ClassClick from "./components/ClassClick";
-import ParentComponent from "./components/ParentComponent";
+import ParentComp from "./components/ParentComp";
+import PureComp from "./components/PureComp";
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet />
-      <Welcome/> */}
-      {/* <Hello name="Bruce"/> */}
-
-      {/* <Greet name="Bruce">
-        <h1>childdd</h1>
-      </Greet>
-      <Greet name ="John"/> */}
-
-      {/* <Welcome>
-          <h3>test child welcome!</h3>
-          <h3>test child welcome!2</h3>
-      </Welcome> */}
-      {/* 
-      <Counter /> */}
-
-      {/* <Destructor name="Johnny1" />
-      <Destructor name="Johnny2" />
-      <Destructor name="Johnny3" /> */}
-
-      <ParentComponent/>
-      {/* <ClassClick/> */}
-
+      <ParentComp />
     </div>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,45 +2,45 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-       count : 0
-    }
+      count: 0,
+    };
   }
 
-  incrementNumber () {
-
+  incrementNumber() {
     // this.setState({
     //     count : this.state.count + 1
     // }, () => {(console.log("this.state.count = " + this.state.count))})
 
-    this.setState( (previousState, props) => ({
-      count : previousState.count + 1
-    }), () => {(console.log("this.state.count = " + this.state.count))}
-    )
-
+    this.setState(
+      (previousState) => ({
+        count: previousState.count + 1,
+      }),
+      () => {
+        console.log("this.state.count = " + this.state.count);
+      }
+    );
   }
 
   incrementByFive() {
-
-    this.incrementNumber()
-    this.incrementNumber()
-    this.incrementNumber()
-    this.incrementNumber()
-    this.incrementNumber()
-
+    this.incrementNumber();
+    this.incrementNumber();
+    this.incrementNumber();
+    this.incrementNumber();
+    this.incrementNumber();
   }
-
-
 
   render() {
     return (
-        <div>
+      <div>
         <div>count is now equal to {this.state.count}</div>
-        <button onClick={() => this.incrementByFive()}>Click To Increment ! </button>
-        </div>
-    )
+        <button onClick={() => this.incrementByFive()}>
+          Click To Increment !{" "}
+        </button>
+      </div>
+    );
   }
 }
 
